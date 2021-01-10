aa6b7219fbd125e8042d6095d0e3a6c163b54855
Maali Hasan <mhasan@asaltech.com>
2021-01-10 15:09:38 +0200

    update dependeny of react-native-rainbow-module

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const blacklist = require('metro-config/src/defaults/blacklist');
 const escape = require('escape-string-regexp');
-const pak = require('../package.json');
+const pak = require('./package.json');
 
 const root = path.resolve(__dirname, '..');
 
@@ -16,11 +16,14 @@ module.exports = {
   // We need to make sure that only one version is loaded for peerDependencies
   // So we blacklist them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    blacklistRE: blacklist(
-      modules.map(
-        (m) =>
-          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
-      )
+    blacklistRE: blacklist([
+      /node_modules\/.*\/node_modules\/react-native\/.*/,
+
+    ]
+      // modules.map(
+      //   (m) =>
+      //     new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
+      // )
     ),
 
     extraNodeModules: modules.reduce((acc, name) => {
