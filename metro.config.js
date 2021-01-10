cb80c4e407677db726e933790a1ec2f01b2b2893
Maali Hasan <mhasan@asaltech.com>
2021-01-10 16:26:40 +0200

    update babel file

--- metro.config.js
+++ metro.config.js
@@ -1,43 +1,29 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
 const path = require('path');
-const blacklist = require('metro-config/src/defaults/blacklist');
-const escape = require('escape-string-regexp');
-const pak = require('./package.json');
-
-const root = path.resolve(__dirname, '..');
-
-const modules = Object.keys({
-  ...pak.peerDependencies,
-});
+const extraNodeModules = {
+  'react-native-rainbow-module': path.resolve('./node_modules/react-native-rainbow-module'),
+};
+const watchFolders = [
+  path.resolve('./node_modules/react-native-rainbow-module')
+];
 
 module.exports = {
-  projectRoot: __dirname,
-  watchFolders: [root],
-
-  // We need to make sure that only one version is loaded for peerDependencies
-  // So we blacklist them at the root, and alias them to the versions in example's node_modules
-  resolver: {
-    blacklistRE: blacklist([
-      /node_modules\/.*\/node_modules\/react-native\/.*/,
-
-    ]
-      // modules.map(
-      //   (m) =>
-      //     new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
-      // )
-    ),
-
-    extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, 'node_modules', name);
-      return acc;
-    }, {}),
-  },
-
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: true,
+        inlineRequires: false,
       },
     }),
   },
+  resolver: {
+    extraNodeModules
+  },
+  watchFolders,
 };
